9704e7a1cdb3887f63ae6cc5b406ba0030554162
constantine <okonemmanuelmma@gmail.com>
2024-09-02 11:53:19 +0100

    initial project

---
+++ createCVComponents/carousel.tsx
@@ -0,0 +1,11 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const AppButton = (props: any) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default AppButton;
