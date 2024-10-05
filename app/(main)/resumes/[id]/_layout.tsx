62f17d1d218a7360b172e8719ea60f5c53e25e94
Jeremiah Lena <jeremiahlena13@gmail.com>
2024-10-05 20:30:08 +0100

    changes

--- app/(main)/resumes/[id]/_layout.tsx
+++ app/(main)/resumes/[id]/_layout.tsx
@@ -1,13 +1,22 @@
+import RobotSVG from "@/assets/icons/robot";
+import Box from "@/components/ui/box";
 import ScreenContainer from "@/components/ui/screen-container";
 import Text from "@/components/ui/text";
 import { useGetResumeByIDQuery } from "@/http/resumeApi";
 import { Resume } from "@/http/types";
+import { Theme } from "@/theme";
+import { backgroundColor, useTheme } from "@shopify/restyle";
 import { Stack, useLocalSearchParams } from "expo-router";
 import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
-import { ActivityIndicator } from "react-native";
+import {
+  ActivityIndicator,
+  Pressable,
+  useWindowDimensions,
+} from "react-native";
 
 export default function ResumeEditLayout() {
+  const { spacing } = useTheme<Theme>();
   const formMethods = useForm<Resume>({
     defaultValues: {
       id: "",
@@ -32,13 +41,13 @@ export default function ResumeEditLayout() {
 
   React.useEffect(() => {
     (async function () {
-      console.log(JSON.stringify(data, null, 3));
       if (data) {
         formMethods.reset({
           ...data.data,
           experiences: data.data.experiences?.map((exp) => ({
             ...exp,
-            startDate: exp.startDate === 'Present' ? "Present" : new Date(exp.startDate),
+            startDate:
+              exp.startDate === "Present" ? "Present" : new Date(exp.startDate),
             endDate: new Date(exp.endDate),
           })),
           education: data.data.education?.map((edu) => ({
@@ -50,6 +59,8 @@ export default function ResumeEditLayout() {
       }
     })();
   }, [data, formMethods]);
+
+  const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = useWindowDimensions();
 
   if (isFetching) {
     return (
@@ -71,6 +82,27 @@ export default function ResumeEditLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="edit/index" />
       </Stack>
+      <Box
+        width={60}
+        height={60}
+        borderRadius={10}
+        position="absolute"
+        zIndex={1000}
+        backgroundColor="primary"
+        bottom={0.1 * DEVICE_HEIGHT}
+        right={spacing.l}
+      >
+        <Pressable
+          style={{
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <RobotSVG />
+        </Pressable>
+      </Box>
     </FormProvider>
   );
 }
